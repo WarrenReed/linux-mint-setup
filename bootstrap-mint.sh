684a3459c3a08f8638ed73e005b2e9ef4ea888ad
Warren Reed <wwreed@gmail.com>
2026-02-22 21:28:31 +0200

    fix(dotnet): export SSL_CERT_DIR for dev-certs trust command

--- bootstrap-mint.sh
+++ bootstrap-mint.sh
@@ -521,6 +521,9 @@ configure_fish() {
     fi
     
     # Trust .NET development certificates
+    # Export SSL_CERT_DIR for the dotnet command in this bash session
+    export SSL_CERT_DIR="/etc/ssl/certs:$HOME/.aspnet/dev-certs/trust"
+    
     if dotnet dev-certs https --check --trust &> /dev/null; then
         print_info ".NET HTTPS development certificate is already trusted."
     else
